7e4041aa002b474f0cf038fc00b5af574ea0a4d9
Bhashithe Abeysinghe <bhashithe@gmail.com>
2019-01-31 14:12:57 -0500

    port

--- Slate/Slate.py
+++ Slate/Slate.py
@@ -294,5 +294,5 @@ def departmenyStats(uni, dept, term, year) :
 
 if __name__ == '__main__':
      app.secret_key='secret123'
-     app.run(debug=True)
+     app.run(debug=True, host='tinman.cs.gsu.edu', port=5015)
 
